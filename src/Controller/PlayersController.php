5b857bbc6d9ec826dc75187b0ed752ca58beaf7b
Theo Folquin <theo.folquin.dev@gmail.com>
2023-04-12 23:14:11 +0200

    Doctrine installed

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -8,12 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PlayersController extends AbstractController
 {
-    #[Route('/players', name: 'app_players')]
-    public function index(): JsonResponse
+    #[Route('/players/{id}', name: 'app_players')]
+    public function index(
+        string $id
+    ): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/PlayersController.php',
+            'id' => $id
         ]);
     }
 }
