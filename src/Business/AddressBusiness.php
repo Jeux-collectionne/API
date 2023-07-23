1f01b247891c2e42adc3f5b0d3994bc239a5dfa6
Theo Folquin <theo.folquin.dev@gmail.com>
2023-07-23 19:41:13 +0200

    wip: address realted functions

--- src/Business/AddressBusiness.php
+++ src/Business/AddressBusiness.php
@@ -2,7 +2,9 @@
 
 namespace App\Business;
 
+use App\Entity\Address;
 use App\Repository\AddressRepository;
+use App\RequestBody\AddressBody;
 use Doctrine\ORM\EntityManagerInterface;
 
 class AddressBusiness
@@ -18,5 +20,27 @@ class AddressBusiness
     public function getAddresses(): array
     {
         return $this->addressRepository->findAll();
+    }
+
+    /**
+     * Modifies the given address with the given infos
+     */
+    public function modifyAddress(AddressBody $addressBody, Address $address)
+    {
+        !$addressBody->getCity()    ?: $address->setCity($addressBody->getCity());
+        !$addressBody->getZipCode() ?: $address->setZipCode($addressBody->getZipCode());
+        !$addressBody->getName()    ?: $address->setName($addressBody->getName());
+
+        $this->em->persist($address);
+        $this->em->flush();
+    }
+
+    /**
+     * Deletes the given address
+     */
+    public function deleteAddress(Address $address): void
+    {
+        $this->addressRepository->remove($address);
+        $this->em->flush();
     }
 }
--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -3,12 +3,12 @@
 namespace App\Controller;
 
 use App\Business\AddressBusiness;
+use App\Entity\Address;
 use App\RequestBody\AddressBody;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
-/** @todo Faire l'appel au business pour les réponses */
 #[Route(path: '/addresses')]
 class AddressController extends AbstractFOSRestController
 {
@@ -25,29 +25,39 @@ class AddressController extends AbstractFOSRestController
     }
 
     #[Route(path: '/{address}', methods: 'GET')]
-    public function getAddress()
+    public function getAddress(Address $address)
     {
-
+        $view = $this->view($address);
+        return $this->handleView($view);
     }
 
-    #[Route(path: '/', methods: 'POST')]
+    /**
+     * @todo Ajouter les validators pour voir si tout est conforme, qu'on a toute les infos nécessaires
+     */
+    #[Route(path: '/{address}', methods: 'POST')]
     #[ParamConverter('addressBody', converter: 'fos_rest.request_body')]
-    public function createAddress(AddressBody $addressBody)
+    public function createAddress(Address $address, AddressBody $addressBody)
     {
-
+        $this->addressBusiness->modifyAddress($addressBody, $address);
+        $view = $this->view();
+        return $this->handleView($view);
     }
 
-    #[Route(path: '/{id}', methods: 'PUT')]
+    #[Route(path: '/{address}', methods: 'PUT')]
     #[ParamConverter('addressBody', converter: 'fos_rest.request_body')]
-    public function modifyAddress(int $id, AddressBody $addressBody)
+    public function modifyAddress(Address $address, AddressBody $addressBody)
     {
-
+        $this->addressBusiness->modifyAddress($addressBody, $address);
+        $view = $this->view();
+        return $this->handleView($view);
     }
 
-    #[Route(path: '/{id}', methods: 'DELETE')]
-    public function deleteAddress(int $id)
+    #[Route(path: '/{address}', methods: 'DELETE')]
+    public function deleteAddress(Address $address)
     {
-        
+        $this->addressBusiness->deleteAddress($address);
+        $view = $this->view();
+        return $this->handleView($view);
     }
     
 }
